40d4d0e3fb199bc733a85f7c99e6933a04a60c76
Igor Lyutoev <ilyutoev@gmail.com>
2017-12-08 22:28:27 +0300

    Add settings_example.py

---
+++ spiders/settings_example.py
@@ -0,0 +1,10 @@
+DATABASE = {
+    'drivername': 'postgresql+psycopg2',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'user',
+    'password': 'password',
+    'database': 'db_name',
+}
+
+SENTRY_PROJECT_URL = 'Your sentry project url'
