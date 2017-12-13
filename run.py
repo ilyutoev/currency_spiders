df91ac147017ad25c7783b7306aa6e36fa1af5a5
Igor Lyutoev <ilyutoev@gmail.com>
2017-12-13 23:05:14 +0300

    Refactor. Centrobank scraping with common spider.

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
+from datetime import date
+from spiders.common_spider import run_spider
+from spiders.response_handlers import cb_response_scraping
 from spiders import spider_cb, spider_sevnb, spider_sberbank, spider_vtb24
 
+
 if __name__ == '__main__':
-    spider_cb.run_spider()
-    spider_sevnb.run_spider()
-    spider_sberbank.run_spider()
-    spider_vtb24.run_spider()
+
+    run_spider(
+        bank_id=1,
+        bank_name='Центробанк',
+        scraping_function=cb_response_scraping,
+        request_url='http://www.cbr.ru/scripts/XML_daily.asp?date_req={}'.format(date.today().strftime('%d/%m/%Y')),
+        request_post=None,
+        request_cookies=None
+    )
+
+    # spider_sevnb.run_spider()
+    # spider_sberbank.run_spider()
+    # spider_vtb24.run_spider()
